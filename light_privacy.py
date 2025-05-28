d7e6c4d79e178446f91505e2acba511e0414cc14
SamyBlk <samybelkessa11@gmail.com>
2025-05-28 18:31:01 +0100

    commit 13

--- light_privacy.py
+++ light_privacy.py
@@ -1,5 +1,14 @@
 import re
 from langchain_experimental.data_anonymizer import PresidioReversibleAnonymizer
+import spacy
+from spacy.cli import download
+
+# Forcer le modèle léger et empêcher l'installation du modèle lourd
+try:
+    spacy.load("en_core_web_sm")
+except OSError:
+    print("[INFO] SpaCy model not found, downloading 'en_core_web_sm'...")
+    download("en_core_web_sm")
 
 # Configuration unique de l'anonymiseur
 anonymizer = PresidioReversibleAnonymizer(
